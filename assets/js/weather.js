53c32bdf60eee5a1f23c35a5e457e0bf190164d1
the-electro-punk <bwr2555@hotmail.com>
2021-09-19 15:06:23 -0500

    tinkered with new codes

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -3,7 +3,7 @@
 // var lon = 86.7816
 
 // APIkey = 004b2d41992a3c0f4120565536b7c749
-// let cities = document.getElementById("citiesChoice")
+
 // let results = cities.options[cities.selectedIndex].value
 // console.log(results)
 
@@ -14,7 +14,7 @@
 // )
 
 var APIkey = '0785e2597394b5d41a6d4cfe9160b61e'
-const searchBTN = document.getElementById('searchBTN')
+// const searchBTN = document.getElementById('searchBTN')
 let cityData = document.getElementById('cityData')
 
 searchBTN.addEventListener('click', searchCity)
@@ -39,6 +39,7 @@ function searchCity () {
         console.log("longitude is " + longitude)
         console.log(response)
         // this uses the latitude and longitude to gather the weather data of the particular city
+        // for some reason, I keep getting bad requests from this API key and suspect it may not be functional
         fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude},&lon=${longitude},&appid=${APIkey}`
         )
         .then(function(response) {
@@ -49,6 +50,24 @@ function searchCity () {
         })
     })
 }
+
+// this was an attempt to get the weather forcast data from just the Lat & Lon API key but still failed
+// function searchCity() {
+    // let cities = document.getElementById("citiesChoice").value
+    // console.log(cities)
+    // if (cities === "Nashville") {
+    //     var lat = 36.1659
+    //     var lon = -86.7844
+    //     fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat},&lon=${lon},&appid=${APIkey}`
+    //     )
+    //     .then(function(response) {
+    //         return response.json();
+    //     })
+    //     .then(function(response) {
+    //         console.log(response + " is go")
+    //     })
+    // } 
+
 
 // fetch(
 //   'https://api.giphy.com/v1/gifs/random?api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN' + serchterm + '&rating=' + rating + '&API_key=004b2d41992a3c0f4120565536b7c749'
