c8a8cf1871f8f08761b49ebef6b25adbc8512fcd
the-electro-punk <bwr2555@hotmail.com>
2021-09-18 20:18:05 -0500

    tried fixing 2nd API key

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -15,12 +15,18 @@
 
 var APIkey = '0785e2597394b5d41a6d4cfe9160b61e'
 const searchBTN = document.getElementById('searchBTN')
+let cityData = document.getElementById('cityData')
 
 searchBTN.addEventListener('click', searchCity)
 
+// this function takes the name of the city entered to gather the weather data
 function searchCity () {
     var cityName = document.getElementById("searchBar").value
+    if (cityName === null) {
+        window.alert("type in a city")
+    }
     console.log(cityName)
+    // this API key obtains the latitude and longitude of the city so to gather the weather forecast
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName},&appid=${APIkey}`
     )
     .then(function(response) {
@@ -31,14 +37,15 @@ function searchCity () {
         var longitude = response[0].lon
         console.log("latitude is " + latitude)
         console.log("longitude is " + longitude)
-        
+        console.log(response)
+        // this uses the latitude and longitude to gather the weather data of the particular city
         fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude},&lon=${longitude},&appid=${APIkey}`
         )
         .then(function(response) {
             return response.json();
         })
         .then(function(response) {
-            console.log("weather" + response)
+            console.log(response + " is go")
         })
     })
 }
